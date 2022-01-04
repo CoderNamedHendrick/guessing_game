a31649c61b34033cc6f1a7b5658c2894e1349a05
hendrick <sebastinesoacatp@gmail.com>
2022-01-04 14:55:21 +0100

    using loops in rust

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
-    println!("Please input your guess.");
+    println!("Guess the number!");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    println!("The secreat number is: {}", secret_number);
 
-    println!("You guess: {}", guess);
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        println!("You guess: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
